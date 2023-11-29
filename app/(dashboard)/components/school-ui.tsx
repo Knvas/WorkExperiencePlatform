3eed50e05c3f6947bb9c50cda99085000eedb8c8
perezcato <perezcatoc@gmail.com>
2023-11-29 15:01:18 +0100

    Added progress bar

--- app/(dashboard)/components/school-ui.tsx
+++ app/(dashboard)/components/school-ui.tsx
@@ -6,10 +6,15 @@ import TeacherIcon from "@/components/icons/teacher";
 import Approved from "@/components/icons/approved";
 import Pending from "@/components/icons/pending";
 import Declined from "@/components/icons/declined";
-import {Button, Card, CardBody, CardHeader, Divider} from "@nextui-org/react";
+import {Button, Card, CardBody, CardHeader, divider, Divider} from "@nextui-org/react";
 import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";
-
 import Progress from "@/components/progress";
+import dynamic from "next/dynamic";
+
+const Plot = dynamic(() => import('react-plotly.js') , {
+  ssr: false
+})
+
 
 const SchoolUi = () => {
   return (
@@ -53,7 +58,7 @@ const SchoolUi = () => {
         </div>
 
       </div>
-      <div className="grid grid-cols-10">
+      <div className="grid grid-cols-10 gap-x-3">
         <Card
           className="col-span-7"
           shadow={'none'}>
@@ -66,9 +71,7 @@ const SchoolUi = () => {
           <CardBody>
             <Table
               fullWidth
-              classNames={{
-                th: ["bg-transparent", "text-default-500", "border-b", "border-divider"],
-              }}
+              classNames={{th: ["bg-transparent", "text-default-500", "border-b", "border-divider"],}}
               removeWrapper
               shadow={'none'}
               className="divide-y divide-y-4"
@@ -91,6 +94,83 @@ const SchoolUi = () => {
           </CardBody>
         </Card>
 
+        <div className="col-span-3 space-y-3">
+          <Card
+            shadow={'none'}
+            className="px-1 border">
+            <CardHeader className="font-semibold">
+              Notifications
+            </CardHeader>
+            <Divider />
+            <CardBody>
+
+            </CardBody>
+          </Card>
+
+          <Card
+            shadow={'none'}
+            className="px-1 border">
+            <CardHeader className="font-semibold justify-center">
+              Student Progress for 2023
+            </CardHeader>
+            <Divider />
+            <CardBody>
+              <div className='scale-150'>
+                <Plot
+                  data={[
+                    {
+                      values: [65, 25,10,],
+                      labels: ['US', 'China', 'European Union'],
+                      domain: {column: 10},
+                      name: 'GHG Emissions',
+                      hoverinfo: 'label+percent+name',
+                      hole: .70,
+                      type: 'pie',
+                      marker: {
+                        colors: ['#0D7D25', '#9747FF', '#CB1F1F'],
+                        line: {
+                          color: 'white',
+                          width: 2,
+                        },
+                      },
+                    }
+                  ]}
+
+                  layout={{
+                    width: 320,
+                    height: 320,
+                    showlegend: false,
+                    annotations: [
+                      {
+                        font: {
+                          size: 8,
+                        },
+                        showarrow: false,
+                        text: `Total Students`,
+                        x: 0.50,
+                        y: 0.55,
+                        align: 'center'
+                      },
+                      {
+                        font: {
+                          size: 16
+                        },
+                        showarrow: false,
+                        text: `10000`,
+                        x: 0.50,
+                        y: 0.45,
+                        align: 'center'
+                      },
+                    ],
+                  }}
+                />
+              </div>
+
+
+            </CardBody>
+          </Card>
+
+        </div>
 
       </div>
     </div>
